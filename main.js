f1f7320736e6cf8f70bf27380194721d7055bf12
Shalini271 <87563847+Shalini271@users.noreply.github.com>
2023-08-22 10:31:09 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,37 @@ drawn_sketch= "";
 answer_holder= "";
 
 score = 0; 
+
+function updateCanvas(){
+background("white");
+}
+
+function setup(){
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+}
+
+function draw(){
+check_sketch();
+if (drawn_sketch==sketch){
+    answer_holder="set";
+    score=score+1;
+    document.getElementById("score").innerHTML="Score: " + score;
+}
+}
+
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML="Time: " + timer_counter;
+    if(timer_counter>800){
+        timer_counter=0;
+        timer_check="completed";
+    }
+    if(timer_check=="completed" || answer_holder=="set"){
+        timer_check="";
+        answer_holder="";
+        updateCanvas();
+    }
+
+}
